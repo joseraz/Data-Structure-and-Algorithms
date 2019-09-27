7315d9367e2170d3dd57e5dd83e6e8663c168160
Joe <joeraz@gmail.com>
2019-09-27 16:33:43 +0100

    Learning about Graphs, Nodes, and Edges

---
+++ 10 Code/Graph theory.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 16:10:30 2019
+
+@author: jose
+"""
+
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+    
